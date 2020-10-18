683c6362a9b46159460b7474888226ce84f836ac
Ayushi1107 <70976770+Ayushi1107@users.noreply.github.com>
2020-10-18 19:45:42 -0400

    Add files via upload

---
+++ Dustbin.js
@@ -0,0 +1,22 @@
+class Dustbin {
+    constructor(x, y, width, height) {
+        var options ={
+            isStatic:true,
+            density:3,
+            friction:1
+        }
+      
+      this.body = Bodies.rectangle(x, y, width, height,options);
+      this.width = width;
+      this.height = height;
+      
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      var angle=this.body.angle;
+      rectMode(CENTER);
+      fill("red");
+      rect(pos.x, pos.y, this.width, this.height);
+    }
+  };
---
+++ Paper.js
@@ -0,0 +1,27 @@
+class Paper {
+    constructor(x, y) {
+    var options = {
+            'density':1.2,
+            
+
+    }
+    this.body = Bodies.circle(x, y,20,options);
+    
+      
+      World.add(world, this.body);
+    }
+
+ display(){
+    var pos =this.body.position;
+    //var angle=this.body.angle;
+    push ();
+    translate(pos.x,pos.y)
+    //rotate(angle);
+    ellipseMode(RADIUS);
+    fill("blue");
+    ellipse(0,0,30,30)
+    pop();
+  
+ } 
+};
+ 
